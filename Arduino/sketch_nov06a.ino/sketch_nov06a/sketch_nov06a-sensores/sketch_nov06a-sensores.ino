3eb94bf83f390c027429526d0bb8f75fd74c1afa
idelfrides <idelfridesjorge@alu.ufc.br>
2017-11-22 14:40:01 -0300

    Atualizando o código do soil moisture sensor

---
+++ Arduino/sketch_nov06a.ino/sketch_nov06a/sketch_nov06a-sensores/sketch_nov06a-sensores.ino
@@ -0,0 +1,25 @@
+int port_soil = A0;
+//int port_dht11 = 2;
+
+int data_soil_pin = 0; 
+//int data_dht11_pin = 0; 
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("SOIL MOISTURE SENSOR");
+  pinMode(port_soil, INPUT);
+  //pinMode(port_dht11, INPUT);
+}
+
+void loop() {
+  delay(5000) // espera por 5 segundos
+  data_soil_pin = analogRead(port_soil);
+  //data_dht11_pin = digitalRead(port_dht11);  
+  Serial.println(data_soil_pin); 
+  Serial.println(data_dht11_pin);  
+  Serial.println("------------------------------")
+}
+
+
+
