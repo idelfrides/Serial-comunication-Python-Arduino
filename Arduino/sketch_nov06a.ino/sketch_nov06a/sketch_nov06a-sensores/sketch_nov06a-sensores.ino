80a21a36514b023d7984dbd897b9306cd3dd313e
idelfrides <idelfridesjorge@alu.ufc.br>
2017-12-06 15:50:29 -0300

    Finished the arduino code for all sensores

--- Arduino/sketch_nov06a.ino/sketch_nov06a/sketch_nov06a-sensores/sketch_nov06a-sensores.ino
+++ Arduino/sketch_nov06a.ino/sketch_nov06a/sketch_nov06a-sensores/sketch_nov06a-sensores.ino
@@ -1,45 +1,69 @@
+/* inclisão de bibliotecas */
+#include "DHT.h"
 
 /* Definição de portas */
-int port_dht = 2;
-int port_umid = 7;
-int port_soil = A0;
+#define DHTPIN 2    // what pin we're connected to
+#define umidPin 2
+#define soilPin A0
+
+/* definindo o tipo de DHT aser utilizado nesta app */
+#define DHTTYPE DHT11  // DHT 11 
 
 /* Definição de pinos de dados */
-int data_dht = 0;
-int data_umid = 0;
-int data_soil = 0;
+float data_dht = 0;
+float data_umid = 0;
+float data_soil = 0;
+float data_dht_f = 0;
+
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht(DHTPIN, DHTTYPE);
+DHT dhtUmid(umidPin, DHTTYPE);
 
 /* Configuração de pinos de entrada de dados e
    taxa de transmissão */
 void setup() {
   Serial.begin(9600);
-  Serial.println("\n\n SOIL MOISTURE SENSOR\n");
-  Serial.print("********************************\n\n");
+  Serial.println("\n\nDHT11 AND SOIL MOISTURE SENSORS\n");
+  Serial.print("****************************************\n\n");
   
-  pinMode(port_dht, INPUT);
-  pinMode(port_umid, INPUT);
-  pinMode(port_soil, INPUT);
+  //pinMode(port_dht, INPUT);
+  pinMode(umidPin, INPUT);
+  pinMode(soilPin, INPUT);
+
+  dht.begin();
+  dhtUmid.begin();
 }
 
 /* Leitura e captura contínua de dados 
    enviados pelos sensores - loop() */
 void loop() {
   /* delay(ms) é dado em milisegundos. 1 ms = 0.001 s */
-  delay(5000); // espera por 5 segundos
+  /* Espera por 5 s entre medidas */
+  delay(5000); 
   
   /* leitura de dados */
-  data_dht = analogRead(port_dht);
-  data_umid = digitalRead(port_umid);  
-  data_soil = analogRead(port_soil);
-  
-  Serial.println(data_dht); 
-  Serial.print("\n\n");
-  Serial.println(data_umid);
-  Serial.println(data_soil);
-  //tone(port_soil, 1, 1);
-  // Serial.println(data_dht11_pin);  
-  // Serial.println("------------------------------");
-}
+  data_dht = dht.readTemperature(); // celcius
+  data_umid = dhtUmid.readHumidity();  
+  data_soil = analogRead(soilPin);
+  data_dht_f = dht.readTemperature(true); //Fahrenheit
 
+  if (isnan(data_dht) || isnan(data_umid) || isnan(data_dht_f)){
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  if(isnan(data_soil)){
+    Serial.println("\n Faile by reading soil moisture! \n");
+    return;
+  }else{
+    Serial.println(data_dht); 
+    delay(2000);
+    Serial.println(data_umid);
+    delay(2000);
+    Serial.println(data_soil);
+    delay(2000); 
+    //  Serial.println(data_dht_f); 
+
+  }
+}
 
 
