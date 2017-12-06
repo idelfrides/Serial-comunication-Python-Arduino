770800ea5225ec945498fca84162f3e89a955050
idelfrides <idelfridesjorge@alu.ufc.br>
2017-12-06 14:25:40 -0300

    Update arduino code for all sensor

--- Arduino/sketch_nov06a.ino/sketch_nov06a/sketch_nov06a-sensores/sketch_nov06a-sensores.ino
+++ Arduino/sketch_nov06a.ino/sketch_nov06a/sketch_nov06a-sensores/sketch_nov06a-sensores.ino
@@ -1,26 +1,44 @@
+
+/* Definição de portas */
+int port_dht = 2;
+int port_umid = 7;
 int port_soil = A0;
-//int port_dht11 = 2;
 
-int data_soil_pin = 0; 
-//int data_dht11_pin = 0; 
+/* Definição de pinos de dados */
+int data_dht = 0;
+int data_umid = 0;
+int data_soil = 0;
 
+/* Configuração de pinos de entrada de dados e
+   taxa de transmissão */
 void setup() {
   Serial.begin(9600);
-
   Serial.println("\n\n SOIL MOISTURE SENSOR\n");
   Serial.print("********************************\n\n");
+  
+  pinMode(port_dht, INPUT);
+  pinMode(port_umid, INPUT);
   pinMode(port_soil, INPUT);
-  //pinMode(port_dht11, INPUT);
 }
 
+/* Leitura e captura contínua de dados 
+   enviados pelos sensores - loop() */
 void loop() {
-  delay(1000); // espera por 5 segundos
-  data_soil_pin = analogRead(port_soil);
-  //data_dht11_pin = digitalRead(port_dht11);  
-  Serial.println(data_soil_pin); 
+  /* delay(ms) é dado em milisegundos. 1 ms = 0.001 s */
+  delay(5000); // espera por 5 segundos
+  
+  /* leitura de dados */
+  data_dht = analogRead(port_dht);
+  data_umid = digitalRead(port_umid);  
+  data_soil = analogRead(port_soil);
+  
+  Serial.println(data_dht); 
+  Serial.print("\n\n");
+  Serial.println(data_umid);
+  Serial.println(data_soil);
   //tone(port_soil, 1, 1);
- // Serial.println(data_dht11_pin);  
-  Serial.println("------------------------------");
+  // Serial.println(data_dht11_pin);  
+  // Serial.println("------------------------------");
 }
 
 
